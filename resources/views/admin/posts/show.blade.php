bbb64e20a4badf784364f9723f3f0112dd13c004
Jacopoguerini <jacopo.guerini1993@gmail.com>
2021-07-26 15:56:46 +0200

    Fix show

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -1,9 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container d-flex flex-column">
-        <h3 class="my-3">{{ $post->title }}</h3>
-        {{-- <a href="{{ route('admin.posts.edit'), $post->id }}" class="btn btn-warning">Modifica</a> --}}
+    <div class="container">
+        <h2 class="my-3">{{ $post->title }}</h2>
+        <h5>{{ $post->slug }}</h5>
+        <a class="btn btn-warning mt-3 mb-3" href="{{ route('admin.posts.edit', $post->id) }}">Modifica</a>
+        <a class="btn btn-secondary ml-3" href="{{ route('admin.posts.index') }}">Elenco articoli</a>
         <p>{{$post->content}}</p>
     </div>
 
