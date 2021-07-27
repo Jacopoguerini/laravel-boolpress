48a82f5fc45029e7c5b9854b695ab054d1d99e7f
Jacopoguerini <jacopo.guerini1993@gmail.com>
2021-07-27 17:25:13 +0200

    Add ol per visualizzare post per categoria

--- resources/views/admin/categories/show.blade.php
+++ resources/views/admin/categories/show.blade.php
@@ -1,5 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
+
+    <div class="container">
+        <h2>{{ $category->name }}</h2>
+
+        <ol class="mt-4">
+            @forelse ($category->posts as $post)
+                <li>
+                    <a href="{{ route('admin.posts.show', $post->id) }}">{{ $post->title }}</a>
+                </li>
+            @empty
+                <li>
+                    <h2>Non ci sono ancora articoli in questa categoria!</h2>    
+                </li>
+            @endforelse
+        </ol>
+    </div>
     
 @endsection
