2306035bcdb38825c839f968884170d0fdf25695
Jacopoguerini <jacopo.guerini1993@gmail.com>
2021-08-02 18:15:27 +0200

    Aggiunta pagina NotFound

---
+++ resources/js/pages/NotFound.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="my-5 text-center">
+        <h2>Errore 404</h2>
+        <h3>Pagina non trovata</h3>
+        <img src="https://acegif.com/wp-content/gif/crying-45.gif" alt="" class="my-2">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NotFound'
+}
+</script>
+
+<style lang="scss" scoped>
+    img {
+        border-radius: 5px;
+        box-shadow: 0 0 5px black;
+    }
+
+</style>
